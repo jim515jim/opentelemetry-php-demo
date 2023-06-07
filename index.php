abac5619660916f086a96596a9d0176ad3db8428
Ban <jim515jim@gmail.com>
2023-06-07 12:43:09 +0800

    fix: rm slim

--- index.php
+++ index.php
@@ -1,69 +1,40 @@
 <?php
-
 use OpenTelemetry\Contrib\Zipkin\Exporter as ZipkinExporter;
 use OpenTelemetry\SDK\Common\Export\Http\PsrTransportFactory;
 use OpenTelemetry\SDK\Trace\SpanProcessor\SimpleSpanProcessor;
 use OpenTelemetry\SDK\Trace\TracerProvider;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Selective\BasePath\BasePathMiddleware;
-use Slim\Factory\AppFactory;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$app = AppFactory::create();
+$transport = PsrTransportFactory::discover()->create('http://zipkin:9411/api/v2/spans', 'application/json');
+$zipkinExporter = new ZipkinExporter($transport);
+$tracerProvider = new TracerProvider(
+    new SimpleSpanProcessor($zipkinExporter)
+);
+$tracer = $tracerProvider->getTracer('io.opentelemetry.contrib.php');
 
-// Add Slim routing middleware
-$app->addRoutingMiddleware();
+$root = $span = $tracer->spanBuilder('root')->startSpan();
+$scope = $span->activate();
 
-// Set the base path to run the app in a subdirectory.
-// This path is used in urlFor().
-$app->add(new BasePathMiddleware($app));
+for ($i = 0; $i < 50; $i++) {
+    // start a span, register some events
+    $span = $tracer->spanBuilder('loop-' . $i)->startSpan();
 
-$app->addErrorMiddleware(true, true, true);
+    $span->setAttribute('remote_ip', '1.2.3.4')
+        ->setAttribute('country', 'USA');
 
-/** routes start */
-$app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write('Hello, World!!!');
-    return $response;
-})->setName('root');
+    $span->addEvent('found_login' . $i, [
+        'id' => $i,
+        'username' => 'otuser' . $i,
+    ]);
+    $span->addEvent('generated_session', [
+        'id' => md5((string)microtime(true)),
+    ]);
 
-$app->get('/zipkin', function (Request $request, Response $response) {
-    $transport = PsrTransportFactory::discover()->create('http://127.0.0.1:9411/api/v2/spans', 'application/json');
-    $zipkinExporter = new ZipkinExporter($transport);
-    $tracerProvider = new TracerProvider(
-        new SimpleSpanProcessor($zipkinExporter)
-    );
-    $tracer = $tracerProvider->getTracer('io.opentelemetry.contrib.php');
+    $span->end();
+}
+$scope->detach();
+$root->end();
 
-    $root = $span = $tracer->spanBuilder('root')->startSpan();
-    $scope = $span->activate();
-
-    for ($i = 0; $i < 50; $i++) {
-        // start a span, register some events
-        $span = $tracer->spanBuilder('loop-' . $i)->startSpan();
-
-        $span->setAttribute('remote_ip', '1.2.3.4')
-            ->setAttribute('country', 'USA');
-
-        $span->addEvent('found_login' . $i, [
-            'id' => $i,
-            'username' => 'otuser' . $i,
-        ]);
-        $span->addEvent('generated_session', [
-            'id' => md5((string)microtime(true)),
-        ]);
-
-        $span->end();
-    }
-    $scope->detach();
-    $root->end();
-
-    $tracerProvider->shutdown();
-    $response->getBody()->write('Starting Zipkin example');
-    return $response;
-});
-/** routes end */
-
-// Run app
-$app->run();
+$tracerProvider->shutdown();
+echo "Zipkin example complete!  See the results at http://localhost:9411/";
